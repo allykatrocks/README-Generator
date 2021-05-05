726ad8f67425b3c88b7dcb0b22a1d864c81165ae
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-05-04 18:50:59 -0700

    adding in questions

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ inquirer.prompt([
     type: 'input',
     name: 'title',
     message: 'What is the title of your README?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Give a brief description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'Table of Contents',
+        message: 'Input your Table of Contents here.'
+    },
+    {
+        type: 'input',
+        name: 'Installation Instructions',
+        message: 'How would a user install this application?'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'How would a user use this application?'
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'What are the guidelines for contribution?'
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'What are the testing guidelines?'
     }
 ]).then((response) => {
       console.log(response);
